5e2c607a1b361f0ef0f467b1188c8068b600d2aa
Bhardwaj4_ <111901770+Dialiser@users.noreply.github.com>
2024-08-03 11:26:36 +0530

    first commit

---
+++ frontend/src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+ import ReactDOM from "react-dom";
+// import App from "./App";
+// import { ContextProvider } from "./redux/Context";
+// import { Provider } from "react-redux";
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     {/* <Provider store={Store}> */}
+//       {/* <ContextProvider> */}
+//         <App />
+//       {/* </ContextProvider> */}
+//     {/* </Provider> */}
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
+const root=ReactDOM.createRoot(document.getElementById("root"))
+root.render(
+  <h1>hi</h1>
+)
